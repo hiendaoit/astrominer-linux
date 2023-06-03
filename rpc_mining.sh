a5104e4c5b28a300ad7a593cd4602d7f78cb7b0e
hiendaoit <72371579+hiendaoit@users.noreply.github.com>
2023-06-03 12:25:28 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmxq0csyl27a7fsxgl5d2 -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10100 -r2 dero-node-orionure-sg.mysrv.cloud:10100 -p rpc;
+    sleep 5;
+done
